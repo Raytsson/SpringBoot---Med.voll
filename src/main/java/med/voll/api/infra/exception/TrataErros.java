29d63fc711667a9e420f1334a6a743dfb78d812e
raytsson <raytssonmoraes@hotmail.com>
2024-05-16 14:36:31 -0300

    projeto finalizado

---
+++ src/main/java/med/voll/api/infra/exception/TrataErros.java
@@ -0,0 +1,38 @@
+package med.voll.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import med.voll.api.domain.ValidacaoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice //informa que é um controlador de erros
+public class TrataErros {
+
+    //quando esse metodo tem que ser chamado?? resposta na anotação
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();    //tenmho que capturar as exceptions que deram erro para informar e dar um retorno para o json
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity regraDeNegocio(ValidacaoException ex){
+      return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+
+    private record  DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
